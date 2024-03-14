aeae7ee9ce981ddb272dde8ba9d0f8e7a3707a7c
s1rojov <sirojoff2727@gmail.com>
2024-03-14 16:18:30 +0500

    basemodal become global component

--- src/boot/main.ts
+++ src/boot/main.ts
@@ -1,7 +1,9 @@
 import { boot } from 'quasar/wrappers';
 import BaseIcon from 'src/components/BaseIcon/index.vue'
+import BaseModal from 'src/components/BaseModal/index.vue'
 import BaseButton from 'src/components/BaseButton/index.vue'
 export default boot(async ({ app }) => {
     app.component('BaseIcon', BaseIcon);
     app.component('BaseButton', BaseButton);
+    app.component('BaseModal', BaseModal);
 });
--- src/views/hr/childrens/leadership/index.vue
+++ src/views/hr/childrens/leadership/index.vue
@@ -24,50 +24,28 @@ function selectedItem(val: string) {
 </script>
 <template>
   <div class="px-7 flex flex-col gap-3 pt-3">
-    <div
-      class="border bg-white border-neutral-400/40 py-3 px-4 rounded-2xl flex items-center justify-between"
-    >
+    <div class="border bg-white border-neutral-400/40 py-3 px-4 rounded-2xl flex items-center justify-between">
       <div>
-        <div
-          class="text-zinc-950 text-lg font-semibold leading-relaxed tracking-wide"
-        >
+        <div class="text-zinc-950 text-lg font-semibold leading-relaxed tracking-wide">
           Rahbariyat
           <sup>3</sup>
         </div>
-        <span
-          class="text-neutral-400 text-xs font-normal leading-normal tracking-wide"
-          >Ushbu bo’limda katalog yaratishingiz</span
-        >
+        <span class="text-neutral-400 text-xs font-normal leading-normal tracking-wide">Ushbu bo’limda katalog
+          yaratishingiz</span>
       </div>
       <div class="flex items-center gap-2">
-        <BaseIcon
-          name="list"
-          class="w-5 cursor-pointer h-5"
-          :class="[isItemList ? 'text-[#C49E5E]' : 'text-neutral-400']"
-          @click="isItemList = true"
-        />
-        <BaseIcon
-          name="card"
-          class="w-5 h-5 cursor-pointer"
-          :class="[!isItemList ? 'text-[#C49E5E]' : 'text-neutral-400']"
-          @click="isItemList = false"
-        />
+        <BaseIcon name="list" class="w-5 cursor-pointer h-5" :class="[isItemList ? 'text-[#C49E5E]' : 'text-neutral-400']"
+          @click="isItemList = true" />
+        <BaseIcon name="card" class="w-5 h-5 cursor-pointer"
+          :class="[!isItemList ? 'text-[#C49E5E]' : 'text-neutral-400']" @click="isItemList = false" />
         <BaseButton @click="isCreateModalOpen = true" label="Yaratish" />
       </div>
     </div>
     <!-- modal -->
-    <CreateLeadershipModal />
-    <NotificationModal
-      :isOpenModal="isOpen"
-      content="O'chirmoqchimisiz?"
-      @selectedBtnClicked="test"
-    >
+    <CreateLeadershipModal />/
+    <NotificationModal :isOpenModal="isOpen" content="O'chirmoqchimisiz?" @selectedBtnClicked="test">
       <template #closeBtn>
-        <BaseIcon
-          name="close"
-          class="w-5 h-5 cursor-pointer"
-          @click="isOpen = false"
-        />
+        <BaseIcon name="close" class="w-5 h-5 cursor-pointer" @click="isOpen = false" />
       </template>
     </NotificationModal>
     <div v-if="!isItemList" class="grid grid-cols-2 gap-5">
